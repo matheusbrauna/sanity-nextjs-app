8485c9fc0dee7852a18c364d1b90b67e6235cfe4
Elielthon (Leto) <letocrazy@hotmail.com>
2024-10-30 15:25:45 -0300

    feat: Adjusting texts and images

--- src/components/sections/offset.tsx
+++ src/components/sections/offset.tsx
@@ -42,7 +42,12 @@ export default async function OffsetSection(props: {
       <div className="container">
         <div className="grid gap-6 md:gap-16 w-11/12 items-center py-6 sm:grid-cols-2 justify-center relative after:content-[''] after:absolute after:top-0 after:right-0 after:h-full after:w-[400%] after:bg-secondary after:-z-[1]">
           <AspectRatio ratio={400 / 400}>
-            <Image src={image ?? ''} alt={imageAlt ?? ''} fill />
+            <Image
+              src={image ?? ''}
+              alt={imageAlt ?? ''}
+              fill
+              className="absolute inset-0 object-contain object-center rounded-md"
+            />
           </AspectRatio>
           <div
             className={cn(
@@ -53,13 +58,23 @@ export default async function OffsetSection(props: {
           >
             <div className="space-y-6">
               <div
-                className="prose
-              prose-h1:text-foreground prose-strong:text-foreground prose-h2:text-foreground prose-h1:text-5xl prose-h1:xl:text-7xl prose-h1:font-semibold prose-h1:tracking-tighter prose-h1:leading-tight
-              prose-h2:text-5xl prose-h2:font-light prose-h2:tracking-tighter prose-h2:leading-tight
-              prose-h3:text-4xl prose-h3:font-light prose-h3:tracking-tighter prose-h3:leading-tight
-              prose-h4:text-3xl prose-h4:font-light prose-h4:tracking-tighter prose-h4:leading-tight
-              prose-p:text-muted-foreground prose-p:md:text-xl/relaxed prose-p:lg:text-base/relaxed prose-p:xl:text-xl/relaxed
-            "
+                className="
+                prose
+                prose-h1:text-foreground
+                prose-h2:text-foreground
+                prose-h3:text-foreground
+                prose-h4:text-foreground
+                prose-h5:text-foreground
+                prose-h6:text-foreground
+                prose-strong:text-foreground
+                prose-h1:text-5xl prose-h1:xl:text-7xl prose-h1:font-semibold prose-h1:tracking-tighter prose-h1:leading-tight
+                prose-h2:text-5xl prose-h2:font-light prose-h2:tracking-tighter prose-h2:leading-tight
+                prose-h3:text-4xl prose-h3:font-light prose-h3:tracking-tighter prose-h3:leading-tight
+                prose-h4:text-3xl prose-h4:font-light prose-h4:tracking-tighter prose-h4:leading-tight
+                prose-h5:text-3xl prose-h5:font-light prose-h5:tracking-tighter prose-h5:leading-tight
+                prose-h6:text-3xl prose-h6:font-light prose-h6:tracking-tighter prose-h6:leading-tight
+                prose-p:text-muted-foreground prose-p:md:text-xl/relaxed prose-p:lg:text-base/relaxed prose-p:xl:text-xl/relaxed
+                "
               >
                 <PortableText value={content!} />
               </div>
