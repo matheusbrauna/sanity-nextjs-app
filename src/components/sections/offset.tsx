e68912bcc81883b9c295de0da1ddf31e9b86ae09
Matheus Braúna <matheusbrauna.contato@gmail.com>
2024-10-30 15:06:38 -0300

    feat: add rounded-md on offset

--- src/components/sections/offset.tsx
+++ src/components/sections/offset.tsx
@@ -40,7 +40,7 @@ export default async function OffsetSection(props: {
       id={String(idSection).slice(1)}
     >
       <div className="container">
-        <div className="grid gap-6 md:gap-16 w-11/12 items-center py-6 sm:grid-cols-2 justify-center relative after:content-[''] after:absolute after:top-0 after:right-0 after:h-full after:w-[400%] after:bg-secondary after:-z-[1]">
+        <div className="grid gap-6 md:gap-16 w-11/12 items-center py-6 sm:grid-cols-2 justify-center relative after:rounded-md after:content-[''] after:absolute after:top-0 after:right-0 after:h-full after:w-[400%] after:bg-secondary after:-z-[1]">
           <AspectRatio ratio={400 / 400}>
             <Image
               src={image ?? ''}
