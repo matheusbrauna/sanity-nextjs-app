83cf2d88b439dcfb11f6cc51fc228533e3c30af2
Elielthon (Leto) <letocrazy@hotmail.com>
2024-11-05 16:10:28 -0300

    fix: text size

--- src/components/sections/cards.tsx
+++ src/components/sections/cards.tsx
@@ -15,6 +15,7 @@ import {
   CardHeader,
   CardTitle,
 } from '../ui/card'
+import ProseText from '../prose'
 
 export default async function CardSection(props: {
   id: string
@@ -39,27 +40,9 @@ export default async function CardSection(props: {
     >
       <div className="container gap-6 items-center justify-center">
         <div className="space-y-6 mb-8 md:mb-16">
-          <div
-            className="
-              prose
-              prose-h1:text-foreground
-              prose-h2:text-foreground
-              prose-h3:text-foreground
-              prose-h4:text-foreground
-              prose-h5:text-foreground
-              prose-h6:text-foreground
-              prose-strong:text-foreground
-              prose-h1:text-5xl prose-h1:xl:text-7xl prose-h1:font-semibold prose-h1:tracking-tighter prose-h1:leading-tight
-              prose-h2:text-5xl prose-h2:font-light prose-h2:tracking-tighter prose-h2:leading-tight
-              prose-h3:text-4xl prose-h3:font-light prose-h3:tracking-tighter prose-h3:leading-tight
-              prose-h4:text-3xl prose-h4:font-light prose-h4:tracking-tighter prose-h4:leading-tight
-              prose-h5:text-3xl prose-h5:font-light prose-h5:tracking-tighter prose-h5:leading-tight
-              prose-h6:text-3xl prose-h6:font-light prose-h6:tracking-tighter prose-h6:leading-tight
-              prose-p:text-muted-foreground prose-p:md:text-xl/relaxed prose-p:lg:text-base/relaxed prose-p:xl:text-xl/relaxed
-            "
-          >
+        <ProseText>
             <PortableText value={content!} />
-          </div>
+          </ProseText>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-center">
           {cardList?.map(card => {
